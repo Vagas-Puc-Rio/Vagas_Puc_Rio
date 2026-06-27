548fb2d455fc003f6f8cb532e6d8178df745ff62
Gabriel Lourenço <gabriellmello.vital@gmail.com>
2026-06-26 23:13:06 -0300

    teste_email_pelo_terminal

--- vagas_pucrio/settings.py
+++ vagas_pucrio/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # Faz o Django imprimir os e-mails no terminal em vez de tentar enviar de verdade
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
